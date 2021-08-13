5fe8f74be1965dcddf40a98d3af212a31d4f5b3b
dantereto <2247@holbertonschool.com>
2021-08-13 19:44:58 +0000

    first file+

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Create a file
+file { 'holberton':
+  ensure  => 'present',
+  content => 'I love Puppet',
+  group   => 'www-data',
+  mode    => '0744',
+  owner   => 'www-data',
+  path    => '/tmp/holberton',
+}
