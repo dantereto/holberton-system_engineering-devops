ab3cb675eb351ad68fbaaf5e7f94fa4961a13868
dantereto <2247@holbertonschool.com>
2021-08-14 16:54:11 +0000

    correct pupet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,14 @@
-#pupet
-include stdlib
-file_line { 'configured to use the private key':
+# Turns off password authentication and uses holberton key
+
+
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => 'IdentityFile ~/.ssh/holberton',
-}
-file_line { ' refuse to authenticate':
-  ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
-  line   => 'PasswordAuthentication=no',
 }
