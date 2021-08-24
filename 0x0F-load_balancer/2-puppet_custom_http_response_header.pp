2c55ff4f66c799fe1eb278389666769a80235162
Ubuntu <ubuntu@gc-2247-web-01-1629326045.c.hbtn-students-instances.internal>
2021-08-24 22:06:43 +0000

    puppet

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#puppet
+
+exec { 'add_header':
+  command  => 'sudo apt-get update;
+  sudo apt-get -y install nginx;
+  sudo sed -i "/server_name _/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-enable/default
+  sudo service nginx restart',
+  provider => 'shell',
+}
