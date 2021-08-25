8e23727ee77e0d357bec897460b6cd8d9299fa2f
dantereto <2247@holbertonschool.com>
2021-08-25 00:31:33 +0000

    puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,10 +1,37 @@
-#!/usr/bin/env bash
-#puppet
+# puppet
+exec { 'update':
+  command  => 'apt-get update',
+  user     => 'root',
+  provider => 'shell'
+}
+->
 
-exec { 'add_header':
-  command  => 'sudo apt-get update;
-  sudo apt-get -y install nginx;
-  sudo sed -i "/server_name _/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-enable/default
-  sudo service nginx restart',
-  provider => 'shell',
-  }
+package { 'nginx':
+  ensure   => present,
+  name     => 'nginx',
+  provider => 'apt'
+}
+->
+
+file { 'index':
+  ensure  => present,
+  path    => '/var/www/html/index.nginx-debian.html',
+  mode    => '0644',
+  owner   => 'root',
+  group   => 'root',
+  content => 'Holberton School'
+}
+->
+
+exec { 'Set X-Served-By':
+  command  => 'sed -i "/server_name _;/a add_header X-Served-By \$hostname;" /etc/nginx/sites-enabled/default',
+  user     => 'root',
+  provider => 'shell'
+}
+->
+
+exec { 'restart nginx':
+  command  => 'service nginx restart',
+  user     => 'root',
+  provider => 'shell'
+}
