1e9d2de58c6260513be266eeb3e64a4d3a25dfb4
dantereto <2247@holbertonschool.com>
2021-09-13 21:18:08 +0000

    search data of students

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,27 +1,31 @@
 #!/usr/bin/python3
-"""script that, using this REST API, for a given employee ID,
-returns information about his/her TODO list progress."""
+"""comment"""
 
 import requests
-from sys import argv
+import sys
 
-if __name__ == "__main__":
-    todos_url = 'https://jsonplaceholder.typicode.com/todos?userId=' + argv[1]
-    todos_response = requests.get(todos_url)
-    user_url = 'https://jsonplaceholder.typicode.com/users/' + argv[1]
-    user_response = requests.get(user_url)
 
-    task_done_list = []
-    task_done = 0
-    lenght = len(todos_response.json())
-    user_name = user_response.json().get('name')
-
-    for user in todos_response.json():
-        if user['completed'] is True:
-            task_done_list.append(user['title'])
-            task_done = task_done + 1
-
-    print('Employee {} is done with tasks({}/{}):\
-        '.format(user_name, task_done, lenght))
-    for task in task_done_list:
-        print('\t {}'.format(task))
+def function():
+    """function"""
+    user = requests.get("https://jsonplaceholder.typicode.com/users")
+    for r_user in user.json():
+        if r_user.get("id") == int(sys.argv[1]):
+            EMPLOYEE_NAME = (r_user.get("name"))
+            break
+    NUMBER_OF_DONE_TASKS = 0
+    TOTAL = 0
+    TASK_TITLE = []
+    todos = requests.get("https://jsonplaceholder.typicode.com/todos")
+    for r_todos in todos.json():
+        if r_todos.get("userId") == int(sys.argv[1]):
+            TOTAL += 1
+            if r_todos.get("completed") is True:
+                NUMBER_OF_DONE_TASKS += 1
+                TASK_TITLE.append(r_todos.get("title"))
+    print("Employee {} is done with tasks({}/{}):".format(EMPLOYEE_NAME,
+                                                          NUMBER_OF_DONE_TASKS,
+                                                          TOTAL))
+    for task in TASK_TITLE:
+        print('\t{}'.format(task))
+if __name__ == '__main__':
+    function()
